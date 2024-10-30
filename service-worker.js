c8674dbd094c7098d7b528e97598ac312dbba9d7
Mojtaba Eskandari <56309749+Mojtabace@users.noreply.github.com>
2024-10-30 15:14:38 +0330

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const cacheName = 'pwa-cache-v1';
+const filesToCache = [
+    '/index.html',
+    '/style.css',
+    '/app.js',
+    '/manifest.json',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cache => {
+                return cache.addAll(filesToCache);
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(keyList.map(key => {
+                if (key !== cacheName) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
