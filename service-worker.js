25c590106c2402f9a01334cf4dfaeefa1f7d7191
Mojtaba Eskandari <56309749+Mojtabace@users.noreply.github.com>
2024-10-30 22:27:41 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -37,3 +37,12 @@ self.addEventListener('fetch', event => {
             })
     );
 });
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js')
+  .then(function(registration) {
+    console.log('Service Worker registered with scope:', registration.scope);
+  })
+  .catch(function(error) {
+    console.log('Service Worker registration failed:', error);
+  });
+}
