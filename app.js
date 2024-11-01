6e5c1c6137767ac8aa8ed78225f01e7af204a2d1
Mojtaba Eskandari <56309749+Mojtabace@users.noreply.github.com>
2024-11-01 18:51:27 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js')
-  .then(function(registration) {
-    console.log('Service Worker registered with scope:', registration.scope);
-  })
-  .catch(function(error) {
-    console.log('Service Worker registration failed:', error);
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then((registration) => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    }).catch((error) => {
+      console.log('Service Worker registration failed:', error);
+    });
   });
 }
